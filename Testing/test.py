edcaa0a4e189166ef19d5bcbea6c9e26524a345d
cegurene <carlos.eguren@edu.uah.es>
2022-02-12 14:07:11 +0100

    Hi

---
+++ Testing/test.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+
+print("It was a dark and stormy night.")
+print("Suddenly a shot rang out!")
+print("""It was a dark and stormy night.
+Suddenly a shot rang out!""")
+
+print("Hi! This is a double quote mark: \" We use it for printing.")
+
+print("This\nis\nmy\nsample.")
